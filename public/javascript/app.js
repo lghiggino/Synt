b764501fe253f5f555690a40bbf132dc536d1882
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-19 22:40:16 -0300

    tone is working- kbd mapping and click events also

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,59 +1,59 @@
 
-// for legacy browsers
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext();
+// // for legacy browsers
+// const AudioContext = window.AudioContext || window.webkitAudioContext;
+// const audioContext = new AudioContext();
 
-// get the audio element
-const audioElement = document.querySelector('audio');
-// pass it into the audio context
-const track = audioContext.createMediaElementSource(audioElement);
+// // get the audio element
+// const audioElement = document.querySelector('audio');
+// // pass it into the audio context
+// const track = audioContext.createMediaElementSource(audioElement);
 
-track.connect(audioContext.destination);
+// track.connect(audioContext.destination);
 
-// select our play button
-const playButton = document.querySelector('button');
+// // select our play button
+// const playButton = document.querySelector('button');
 
-playButton.addEventListener('click', function() {
+// playButton.addEventListener('click', function() {
 
-    // check if context is in suspended state (autoplay policy)
-    if (audioContext.state === 'suspended') {
-        audioContext.resume();
-    }
+//     // check if context is in suspended state (autoplay policy)
+//     if (audioContext.state === 'suspended') {
+//         audioContext.resume();
+//     }
 
-    // play or pause track depending on state
-    if (this.dataset.playing === 'false') {
-        audioElement.play();
-        this.dataset.playing = 'true';
-    } else if (this.dataset.playing === 'true') {
-        audioElement.pause();
-        this.dataset.playing = 'false';
-    }
+//     // play or pause track depending on state
+//     if (this.dataset.playing === 'false') {
+//         audioElement.play();
+//         this.dataset.playing = 'true';
+//     } else if (this.dataset.playing === 'true') {
+//         audioElement.pause();
+//         this.dataset.playing = 'false';
+//     }
 
-}, false);
+// }, false);
 
-//reset the audio button state
-audioElement.addEventListener('ended', () => {
-    playButton.dataset.playing = 'false';
-}, false);
+// //reset the audio button state
+// audioElement.addEventListener('ended', () => {
+//     playButton.dataset.playing = 'false';
+// }, false);
 
-//adding Gain (volume)
-const gainNode = audioContext.createGain();
-track.connect(gainNode).connect(audioContext.destination);
+// //adding Gain (volume)
+// const gainNode = audioContext.createGain();
+// track.connect(gainNode).connect(audioContext.destination);
 
-const volumeControl = document.querySelector('#volume');
+// const volumeControl = document.querySelector('#volume');
 
-volumeControl.addEventListener('input', function() {
-    gainNode.gain.value = this.value;
-}, false);
+// volumeControl.addEventListener('input', function() {
+//     gainNode.gain.value = this.value;
+// }, false);
 
-//adding pan
-const pannerOptions = { pan: 0 };
-const panner = new StereoPannerNode(audioContext, pannerOptions);
+// //adding pan
+// const pannerOptions = { pan: 0 };
+// const panner = new StereoPannerNode(audioContext, pannerOptions);
 
-const pannerControl = document.querySelector('#panner');
+// const pannerControl = document.querySelector('#panner');
 
-pannerControl.addEventListener('input', function() {
-    panner.pan.value = this.value;
-}, false);
+// pannerControl.addEventListener('input', function() {
+//     panner.pan.value = this.value;
+// }, false);
 
-track.connect(gainNode).connect(panner).connect(audioContext.destination);
+// track.connect(gainNode).connect(panner).connect(audioContext.destination);
---
+++ public/javascript/index.js
@@ -0,0 +1,46 @@
+const synth = new Tone.Synth().toDestination();
+const keys = document.querySelectorAll(".key")
+
+keys.forEach(key => {
+    key.addEventListener("click", (e)=> {
+        console.log(e.target.id)
+        synth.triggerAttackRelease(e.target.id, "4an");
+    })
+})
+
+const keyboardMap = {
+    "KeyQ":"c3" ,
+    "KeyW":"c#3",
+    "KeyE":"d3" ,
+    "KeyR":"d#3",
+    "KeyT":"e3" ,
+    "KeyY":"f3" ,
+    "KeyU":"f#3",
+    "KeyI":"g3" ,
+    "KeyO":"g#3",
+    "KeyP":"a3" ,
+    "BracketLeft":"a#3",
+    "BracketRight":"b3" ,
+    "KeyA":"c4" ,
+    "KeyS":"c#4",
+    "KeyD":"d4" ,
+    "KeyF":"d#4",
+    "KeyG":"e4" ,
+    "KeyH":"f4" ,
+    "KeyJ":"f#4",
+    "KeyK":"g4" ,
+    "KeyL":"g#4",
+    "Semicolon":"a4" ,
+    "Quote":"a#4",
+    "Backslash":"b4" ,
+}
+
+window.addEventListener("keydown", (e) => {
+    synth.triggerAttackRelease(keyboardMap[e.code], "16an")
+})
+
+// const now = Tone.now()
+// // trigger the attack immediately
+// synth.triggerAttackRelease("C4", "8n", now)
+// synth.triggerAttackRelease("E4", "8n", now + 0.5)
+// synth.triggerAttackRelease("G4", "8n", now + 1)
