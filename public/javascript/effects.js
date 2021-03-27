fbea811f7513cd0d0a1c1e99f26cb5499bd85fdf
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-03-27 13:14:45 -0300

    added gitignore and new assets

--- public/javascript/effects.js
+++ public/javascript/effects.js
@@ -1,11 +1,19 @@
 //distortion
-const distortionControl = document.querySelector("#distortion-effect")
+const distortionControl = document.querySelector("#distortion-effect");
 const distortion = new Tone.Distortion(0).toDestination();
 
 distortionControl.addEventListener("change", (e) => {
     let distortionValue = Number(document.querySelector("#distortion-effect").value)
-    console.log(distortionValue)
+    // console.log(distortionValue);
     distortion["_distortion"] = distortionValue
     synth.connect(distortion);
 })
 
+//oscillator
+const osc = new Tone.Oscillator().toDestination();
+// start at "C4"
+osc.frequency.value = "C4";
+// ramp to "C2" over 2 seconds
+osc.frequency.rampTo("C2", 2);
+// start the oscillator for 2 seconds
+//osc.start().stop("+3");
